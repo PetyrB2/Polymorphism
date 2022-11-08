b4927f16528249e0469045f0dcf52583476e3a80
PetyrB2 <peter.i.broadbent@gmail.com>
2022-11-08 11:52:06 +0000

    Added For loop

--- src/Poly/Polymorphism.java
+++ src/Poly/Polymorphism.java
@@ -2,6 +2,11 @@ package Poly;
 
 // A few classes to work with Bird, Robin, Swallow and Pelican
 class Bird {
+	// Has a Name
+	public void name() {
+		System.out.println("A Bird");
+	}
+
 	// hasFeathers() Method
 	public void hasFeathers() {
 		System.out.println("Has Feathers.");
@@ -20,25 +25,39 @@ class Bird {
 
 // Robin class
 class Robin extends Bird {
-
+	// Robin extends Bird but we Override the name() method locally.
+	@Override
+	public void name() {
+		System.out.println("A Robin");
+	}
 }
 
 // Sparrow class
 class Sparrow extends Bird {
-
+	// Sparrow extends Bird but we Override the name() method locally.
+	@Override
+	public void name() {
+		System.out.println("A Sparrow");
+	}
 }
 
 // Pelican class
 class Pelican extends Bird {
-	// This extends Bird but we Override sing() and size() methods locally.
+	// Pelican extends Bird but we Override name(), sing() and size() methods
+	// locally.
+	@Override
+	public void name() {
+		System.out.println("A Pelican");
+	}
+
 	@Override
 	public void sing() {
-		System.out.println("Is Big in size");
+		System.out.println("Sings Caaaw! Caaaw! Caaaw!");
 	}
 
 	@Override
 	public void size() {
-		System.out.println("Sings Caaaw! Caaaw! Caaaw!");
+		System.out.println("Is Big in size");
 	}
 
 }
@@ -54,7 +73,6 @@ public class Polymorphism {
 		System.out.println("Author: Peter Broadbent");
 		System.out.println("Language: Java \n" + "Version: " + System.getProperty("java.version"));
 		System.out.println("========================\n");
-		System.out.println("");
 
 		// Create a Bird
 		Bird b = new Bird();
@@ -85,10 +103,34 @@ public class Polymorphism {
 		System.out.println("\n----------------------------------------------------------");
 		System.out.println("This has been a basic exmaple of Polymorphism.");
 		System.out.println("All the Birds are alike in that they extend Bird.");
-		System.out.println("The Robin and Swallow classes hold very little data");
+		System.out.println("The Robin and Sparrow classes hold very little data");
 		System.out.println("They inherit the data from bird.");
 		System.out.println("Pelican however does extend Bird but it Overrides size()");
 		System.out.println("and sing() methods in its class as they differ.");
 		System.out.println("----------------------------------------------------------");
+
+
+		// Second way of Printing the data out.
+		System.out.println("----------------------------");
+		System.out.println("Using an Array and For Loop");
+		System.out.println("----------------------------");
+		System.out.println("");
+
+		Bird birds[] = new Bird[4];
+		birds[0] = new Robin();
+		birds[1] = new Sparrow();
+		birds[2] = new Pelican();
+		birds[3] = new Bird();
+
+		// For loop to iterate through the data
+		for (int a = 0; a < birds.length; a++) {
+			System.out.println("\n-------------------------");
+			birds[a].name();
+			birds[a].hasFeathers();
+			birds[a].sing();
+			birds[a].size();
+		}
+
+		System.out.println("\n-------------------------");
 	}
 }
